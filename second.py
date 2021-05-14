43762e3bb73e53365c89d82baaf6a80edb8aee91
Chanran33 <k55416325@gmail.com>
2021-05-14 20:00:37 +0900

    파이썬 파일들

---
+++ second.py
@@ -0,0 +1,13 @@
+#정수형 나누기
+# num1 = float(input("여기에 첫번째 정수를 입력하세요: "))
+# num2 = float(input("여기에 두번째 정수를 입력하세요: "))
+
+# # print(num1 // num2) #몫만 구하기
+# # print(num1 % num2) #나머지만 구하기
+# # print(num1 / num2) #몫과 나머지 구하기
+
+# print(num1+num2)
+
+a = "배고파"
+print(a*3)
+
